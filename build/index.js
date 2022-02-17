b9007b6d13149e7e7c9bad6ce5547d25885e8a1a
Rana Badr <rana_badr30@yahoo.com>
2022-02-17 16:03:59 +0200

    Test typescript

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var numArr = [3, 4, 5, 6];
+var multiply = function (num1, num2) {
+    return num1 * num2;
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const numArr = [3, 4, 5, 6]
+
+const multiply = (num1: number, num2: number): number => {
+    return num1 * num2;
+};
