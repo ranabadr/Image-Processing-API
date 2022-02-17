9b7950cbc39ae6e4f9a9840c870ad872db3d6094
Rana Badr <rana_badr30@yahoo.com>
2022-02-17 16:20:07 +0200

    Build a server

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { response } from 'express';
+
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port, ()=>{
+    console.log(`server running on: http://localhost${port}`);
+});
