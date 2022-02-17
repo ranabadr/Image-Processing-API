5009e7a002d6b32ef320df439828a5a7f2216ff9
Rana Badr <rana_badr30@yahoo.com>
2022-02-17 17:08:46 +0200

    Import File in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { response } from 'express';
 import routes from './routes/index';
+import File from './fileSystem';
+
 
 const app = express();
 const port = 3000;
 
 app.use(routes);
 
-app.listen(port, ()=>{
-    console.log(`server running on: http://localhost${port}`);
-});
+app.listen(port, async (): Promise<void> => {
+    await File.createThumbPath();
+  
+    console.log(`Server run on: http://localhost:${port}`);
+  });
