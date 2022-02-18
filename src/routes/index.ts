a677f02698d04215ba8ee316b148bca9eec3fe26
Rana Badr <rana_badr30@yahoo.com>
2022-02-18 02:42:14 +0200

    Provide the endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,7 @@ const routes: express.Router = express.Router();
 routes.use('/api/pics', pics);
 
 routes.get('/',(req: express.Request, res: express.Response): void => {
-    res.send('Start processing');
+    res.send('Start processing: Listening at <code><a href="/api/pics">/api/pics</a></code>');
   }
 );
 
