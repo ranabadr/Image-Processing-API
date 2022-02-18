f4450f071516a1e97387e934d9269fa797e6ee6b
Rana Badr <rana_badr30@yahoo.com>
2022-02-18 03:16:08 +0200

    Create a fileSystemSpec file

---
+++ src/tests/fileSystemSpec.ts
@@ -0,0 +1,42 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import File from './../fileSystem';
+
+describe('Test image processing', (): void => {
+  it('invalid width value', async (): Promise<void> => {
+    const error: null | string = await File.createThumb({
+      filename: 'foo',
+      width: '-100',
+      height: '500'
+    });
+    expect(error).not.toBeNull();
+  });
+
+  it('filename does not exist', async (): Promise<void> => {
+    const error: null | string = await File.createThumb({
+      filename: 'foo',
+      width: '100',
+      height: '500'
+    });
+    expect(error).not.toBeNull();
+  });
+
+  it('succeeds to write resized thumb file (existing file, valid size values)', async (): Promise<void> => {
+    await File.createThumb({ filename: 'fjord', width: '99', height: '99' });
+
+    const resizedImagePath: string = path.resolve(
+      File.imagesThumbPath,
+      `fjord-99x99.jpg`
+    );
+    let errorFile: null | string = '';
+
+    try {
+      await fs.access(resizedImagePath);
+      errorFile = null;
+    } catch {
+      errorFile = 'File was not created';
+    }
+
+    expect(errorFile).toBeNull();
+  });
+});
